12ae9fe3b3e35adcd0fd89c1733f3f211515f906
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-04-19 14:01:45 -0400

    Add initial seeds for data base

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Project.create(name: 'Cultura Dev', description: 'Blog about my journey into coding')
+Project.create(name: 'Hattrick', description: 'Final project at Ironhack')
+Project.create(name: 'El Nene', description: 'Scion FR-S modification')
+Project.create(name: 'Ectomorfosis', description: 'Blog about fitness')
+Project.create(name: 'Apartamento 7', description: 'Live streaming show')
+Project.create(name: 'Camaleon Arte Visual', description: 'Photography studio in Nicaragua')
+Project.create(name: 'Caprina', description: 'Gourmet cheese store in Nicaragua')
+Project.create(name: 'Cultura Beta', description: 'Blog about tech and games')
+Project.create(name: 'Chacuatol', description: 'Web application for Nicaraguan slang')
+Project.create(name: 'Latino Aventurero', description: 'Youtube channel about my journey in the U.S')
