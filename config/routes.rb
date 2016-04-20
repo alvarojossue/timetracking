5d9aef44492f56fab4166a59b0283b44bd6c23ef
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-04-20 10:53:31 -0400

    Add feature to create new projects

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,21 @@ Rails.application.routes.draw do
 
   get '/', to: 'site#home'
 
-  get '/contact', to: 'site#contact'
+  # get '/contact', to: 'site#contact'
 
-  get '/say_name/:name', to: 'site#say_name'
+  # get '/say_name/:name', to: 'site#say_name'
 
-  get '/calculator', to: 'site#calculator'
+  # get '/calculator', to: 'site#calculator'
 
-  post '/calculate', to: 'site#calculate'
+  # post '/calculate', to: 'site#calculate'
 
   get '/projects', to: 'projects#index'
 
+  post '/projects', to: 'projects#create'
+
+  get '/projects/new', to: 'projects#new'
+
   get '/projects/:id', to: 'projects#show'
+
 
 end
