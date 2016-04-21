ab9729aa69cb0406653926701df55a817cce0e68
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-04-21 12:34:03 -0400

    Add edit feature for projects and time entries

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -13,13 +13,34 @@ class TimeEntriesController < ApplicationController
 
 	def create
 		@my_project = Project.find(params[:project_id])
-		@my_entry = @my_project.time_entries.new(hours: params[:time_entry][:hours], minutes: params[:time_entry][:minutes], date: params[:time_entry][:date])
+		@my_entry = @my_project.time_entries.new(entry_params)
 		if @my_entry.save
 			redirect_to action: 'index', controller: 'time_entries', project_id:
 		@my_project.id
 		else
 			render 'new'
 		end
+	end
 
+	def edit
+		@my_project = Project.find(params[:project_id])
+		@my_entry = @my_project.time_entries.find(params[:id])
+	end
+
+	def update
+		@my_project = Project.find_by(id: params[:project_id])
+		@my_entry = @my_project.time_entries.find_by(id: params[:id])
+		if @my_entry.update(entry_params)
+			redirect_to action: 'index', controller: 'time_entries', project_id: @my_project.id
+		else
+			render 'edit'
+		end
+
+	end
+
+	private
+
+	def entry_params
+		params.require(:time_entry).permit(:hours, :minutes, :date, :comment)
 	end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,11 @@
 class Project < ActiveRecord::Base
 	has_many :time_entries
 	validates :name, presence: true, uniqueness: true, length: {maximum: 10}, format: {with: /\[a-zA-Z0-9]/}
+
+def self.clean_old
+	old_projects = Project.where("created_at < ?", 1.week.ago)
+	old_projects.destroy_all
 end
+
+end
+
